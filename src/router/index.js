e64fca93233932048ee468b10949fc57c8704e5c
Gab2118 <gab-ptp@hotmail.fr>
2022-10-21 15:53:59 -0400

    correctionprobBuild

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import ListView from "../views/ListView.vue";
 import SettingView from "../views/SettingsView.vue";
 
